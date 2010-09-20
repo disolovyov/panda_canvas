acd53bce081b2f72fae4637100ad5cf8fd12b1d8
dimituri <dimituri@gmail.com>
2010-09-20 12:50:00 +0300

    Added example

---
+++ examples/torus.rb
@@ -0,0 +1,30 @@
+require File.expand_path('../../lib/panda_canvas', __FILE__)
+
+PandaCanvas.draw do
+
+  def pix3d(x, y, z)
+    yy = 480 / 2 - z + x / 2 + y / 2
+    xx = 640 / 2 + (x - y) * (3 ** 0.5) / 2
+    pixel xx.to_i, yy.to_i, :color => :white
+  end
+
+  R = 100
+  r = 30
+
+  u = 0
+  while u < 6.28 do
+    t = 0
+    while t < 6.28
+      x = (R + r * Math.cos(u)) * Math.sin(t)
+      y = (R + r * Math.cos(u)) * Math.cos(t)
+      z = r * Math.sin(u)
+      pix3d x, y, z
+      t += 0.1
+    end
+    Fiber.yield
+    u += 0.1
+  end
+
+  pix3d 0, 0, 0
+
+end
